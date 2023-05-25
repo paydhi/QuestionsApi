18eab40b22cd512eeb9d3decd1df199701a5b143
Paydhi <paydhi@gmail.com>
2023-05-25 21:46:37 +0600

    Changed settings.py for PROD, slight changes for "api/questions/"'s OPTIONS method

--- questions_api/questions/views.py
+++ questions_api/questions/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -7,5 +5,11 @@ from rest_framework.response import Response
 class QuestionsViewSet(APIView):
     def post(self, request):
         questions_num = request.data.get('questions_num')
-
         return Response({'message': 'Success', 'questions_num': questions_num})
+
+    def options(self, request, *args, **kwargs):
+        return Response({'message': 'Success',
+                         'expect': {'questions_num': 'int'},
+                         'return': {
+                             'message': 'Success',
+                             'questions_num': 'int'}})
--- questions_api/questions_api/settings.py
+++ questions_api/questions_api/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d#+s1p^h2g21jz6_&@tpe^%s_ips8*rfv@*9o!k29pgikt+@e^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
