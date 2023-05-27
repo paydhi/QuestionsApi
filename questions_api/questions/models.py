8e44483ca84c21a6de343f23f54bcd0da16ef7e4
Paydhi <paydhi@gmail.com>
2023-05-27 23:38:21 +0600

    Created model, changed api/questions/ endpoint to retrieve questions and save them to DB, without validation

--- questions_api/questions/models.py
+++ questions_api/questions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    jservice_id = models.IntegerField(unique=True, blank=False, null=False)
+    answer = models.TextField(blank=False, null=False)
+    question = models.TextField(blank=False, null=False)
+    question_created_at = models.DateTimeField(auto_now=False, blank=False, null=False)
+
--- questions_api/questions/views.py
+++ questions_api/questions/views.py
@@ -1,11 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import requests
+
+from questions.models import Question
+
 
 
 class QuestionsViewSet(APIView):
     def post(self, request):
-        questions_num = request.data.get('questions_num')
-        return Response({'message': 'Success', 'questions_num': questions_num})
+        if not request.data.get('questions_num'):
+            return Response.status_code(400)
+        elif not isinstance(request.data.get('questions_num'), int):
+            return Response.status_code(400)
+        else:
+            questions = requests.get(f"https://jservice.io/api/random?count={request.data.get('questions_num')}")
+            questions = questions.json()
+            for question in questions:
+                question_for_db = {
+                        'jservice_id': question['id'],
+                        'answer': question['answer'],
+                        'question': question['question'],
+                        'question_created_at': question['created_at'],
+                    }
+                question_for_db = Question(**question_for_db)
+                question_for_db.save()
+            return Response(questions)
+
 
     def options(self, request, *args, **kwargs):
         return Response({'message': 'Success',
