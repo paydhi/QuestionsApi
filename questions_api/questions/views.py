40fdbec22606adf3918715bad2b8f6e22401062f
Paydhi <paydhi@gmail.com>
2023-06-04 22:35:11 +0600

    Cleanup

--- questions_api/questions/views.py
+++ questions_api/questions/views.py
@@ -1,5 +1,5 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from questions.models import Questions
 from questions.serializers import QuestionsSerializer, QuestionsPostSerializer
